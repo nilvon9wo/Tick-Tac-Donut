256b28ca5f619e46157b2f41d4e9c032ff90ac9f
Brian <kessler.bm@gmail.com>
2016-09-18 20:05:44 +0200

    deleted js files which the IDE created

--- src/common/PlayerType.js
+++
@@ -1,7 +0,0 @@
-var PlayerType;
-(function (PlayerType) {
-    PlayerType[PlayerType["HUMAN"] = 0] = "HUMAN";
-    PlayerType[PlayerType["COMPUTER"] = 1] = "COMPUTER";
-})(PlayerType || (PlayerType = {}));
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = PlayerType;
