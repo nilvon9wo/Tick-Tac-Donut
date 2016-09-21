1de89deb8abbfbad4e357119f781ab819caec33a
Brian <kessler.bm@gmail.com>
2016-09-21 09:26:47 +0200

    started working on unit tests

--- tests/unit/client/ticTacToe/board/TicTacToeBadLocationErrorTest.ts
+++ tests/unit/client/ticTacToe/board/TicTacToeBadLocationErrorTest.ts
@@ -1,6 +1,4 @@
-/// <reference path='../../../../../declarations/jasmine/jasmine.d.ts' />     
 /// <reference path='../../../../../declarations/Jasmine-Jquery/Jasmine-Jquery.d.ts' />      
-/// <reference path='../../../../../declarations/karma/karma.d.ts' />        
       
 import TicTacToeBadLocationError from '../../../../../src/client/ticTacToe/board/TicTacToeBadLocationError.ts';     
 
@@ -10,12 +8,12 @@ class TestClass {
     }
 }
 
-describe( "TicTacToeBadLocationError", () => {      
+describe('TicTacToeBadLocationError', () => {      
     
-    it( "should extend error and throw message to the super constructor", () => {     
+    it('should extend error and throw message to the super constructor', () => {     
         // Arrange        
         const testClass = new TestClass();
-        const testMessage = "Test Message";
+        const testMessage = 'Test Message';
         let errorCaught = false;
         let errorMessage: String;
 
