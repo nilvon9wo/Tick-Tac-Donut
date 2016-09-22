ef175a20e7fc95f0eb31fd8ee7723cee762d0f36
Brian <kessler.bm@gmail.com>
2016-09-22 08:32:03 +0200

    making new angularjs2 template for tic-tac-toe board

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,29 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  selector: 'tic-tac-toe',
+  template:`
+    <h2>{{hero.name}} details!</h2>
+    <div><label>name: </label>{{hero.name}}</div>
+    <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
+  
+    <div class="ticTacToe--board">
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[0]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[1]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[2]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[3]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[4]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[5]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[6]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[7]}}</div>
+        <div class="ticTacToe--board-cell--background ticTacToe--board-cell--empty">{{cells[8]}}</div>
+    </div>
+      `
 })
-export class AppComponent { }
+export class AppComponent { 
+    title = 'Tick \'Tack Donuts';
+    cells = ['E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'];
+    hero = {name:'Fred'};
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,11 @@
 import { AppComponent }   from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { NgModule }      from '@angular/core';
 
 @NgModule({
   bootstrap:    [ AppComponent ],
   declarations: [ AppComponent ],
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule, FormsModule ]
 })
 export class AppModule { }
