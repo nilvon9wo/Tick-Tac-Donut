dffb7f1ae27e3a7ecfc6c6e5b2e52c1b806ca6b9
Brian <kessler.bm@gmail.com>
2016-09-25 12:11:47 +0200

    completed tests for winning scenarios

--- app/adjudicator.service.spec.ts
+++ app/adjudicator.service.spec.ts
@@ -33,8 +33,58 @@ describe("AdjudicatorService", () => {
 
     
     describe("judge", () => {
+        // Positive (Winning) Scenarios
         it("should declare X the winner when X is in positions 0, 1, and 2", () => {
             playerShouldWinTest(Marker.X, [0,1,2]);
         });
+        
+        it("should declare O the winner when O is in positions 0, 3, and 6", () => {
+            playerShouldWinTest(Marker.O, [0,3,6]);
+        });
+        
+        it("should declare X the winner when X is in positions 0, 4, and 8", () => {
+            playerShouldWinTest(Marker.X, [0,4,8]);
+        });
+        
+        it("should declare O the winner when O is in positions 1, 4, and 7", () => {
+            playerShouldWinTest(Marker.O, [1,4,7]);
+        });
+        
+        it("should declare X the winner when X is in positions 2, 4, and 6", () => {
+            playerShouldWinTest(Marker.X, [2,4,6]);
+        });
+        
+        it("should declare O the winner when O is in positions 2, 5, and 8", () => {
+            playerShouldWinTest(Marker.O, [2,5,8]);
+        });
+        
+        it("should declare X the winner when X is in positions 3, 4, and 5", () => {
+            playerShouldWinTest(Marker.X, [3,4,5]);
+        });
+        
+        it("should declare O the winner when O is in positions 6, 7, and 8", () => {
+            playerShouldWinTest(Marker.O, [6,7,8]);
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     });
 });
