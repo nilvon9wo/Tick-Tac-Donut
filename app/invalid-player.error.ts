c5168c49e17ffbfbe013c869e54d37c587e13607
Brian <kessler.bm@gmail.com>
2016-09-25 14:03:43 +0200

    created tests for announcer service

--- app/invalid-player.error.ts
+++
@@ -1,8 +0,0 @@
-class InvalidPlayerError extends Error {
-    public name = 'InvalidPlayerError';
-    constructor(public message: string) {
-        super(message);
-    }
-}
-
-export default InvalidPlayerError;
