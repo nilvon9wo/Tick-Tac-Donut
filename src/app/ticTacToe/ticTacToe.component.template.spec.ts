cfb1326449d4182d89c3dff618c37c8c41a70252
Brian <kessler.bm@gmail.com>
2016-10-02 20:56:46 +0200

    created tests for tic-tac-toe component

---
+++ src/app/ticTacToe/ticTacToe.component.template.spec.ts
@@ -0,0 +1,37 @@
+import { async, TestBed } from '@angular/core/testing';
+import { BoardComponent } from '../board/board.component';
+import { CellComponent } from '../cells/cell.component';
+import { CellsDaoService } from '../cells/cells-dao.service';
+import { TicTacToeComponent } from './ticTacToe.component';
+import Marker from '../etc/marker.enum';
+
+class MockCellsDaoService extends CellsDaoService { }
+
+let mockCellsDaoService: MockCellsDaoService;
+let componentUnderTest: TicTacToeComponent;
+
+describe( 'TicTacToeComponent', () => {
+
+    describe( 'template', () => {
+        it( 'should render a board', async(() => {
+            TestBed.configureTestingModule( {
+                declarations: [TicTacToeComponent, BoardComponent, CellComponent]
+            });
+
+            TestBed.compileComponents().then(() => {
+                const fixture = TestBed
+                    .overrideComponent( TicTacToeComponent, {
+                        set: {
+                            providers: [
+                                { provide: CellsDaoService, useClass: MockCellsDaoService }
+                            ]
+                        }
+                    })
+                    .createComponent( TicTacToeComponent );
+
+                const componentUnderTest = fixture.nativeElement;
+                expect( componentUnderTest.querySelectorAll( 'board' ).length ).toBe( 1 );
+            });
+        }) );
+    });
+});
