6092c5735342b515a1cab1bb55f97acb62714637
Brian <kessler.bm@gmail.com>
2016-10-02 18:16:02 +0200

    Added a reset button (html only); reset button and board coexist on new ticTacToe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CellComponent }   from './cells/cell.component';
 import { FormsModule }   from '@angular/forms';
 import { NgModule }      from '@angular/core';
+import { TicTacToeComponent }   from './ticTacToe/ticTacToe.component';
 
 @NgModule({
-  bootstrap:    [ BoardComponent ],
-  declarations: [ BoardComponent, CellComponent ],
+  bootstrap:    [ TicTacToeComponent ],
+  declarations: [ TicTacToeComponent, BoardComponent, CellComponent ],
   imports:      [ BrowserModule, FormsModule ]
 })
 export class AppModule { }
---
+++ src/app/ticTacToe/ticTacToe.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component( {
+    selector: 'tic-tac-toe',
+//    styles: [require('./ticTacToe.component.css')],
+    template: require('./ticTacToe.component.html')
+})
+
+export class TicTacToeComponent {}
